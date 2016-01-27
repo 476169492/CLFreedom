4b7b508298c8ad18f07023deca9a0f2bfd27b345
陈磊 <chenlei2447@xunfang.com>
2016-01-27 14:54:35 +0800

    上传想freedom项目

---
+++ CLFreedom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CLFreedom
+//
+//  Created by chenlei on 16/1/27.
+//  Copyright © 2016年 laosiji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
