4abd46f1d6d2fb04e0b71796bb321400d35bea8a
陈磊 <chenlei2447@xunfang.com>
2016-01-27 16:25:05 +0800

    添加 首页滚动类

---
+++ CLFreedom/Classes/ViewControllers/CLHomeTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CLHomeTableViewController.swift
+//  CLFreedom
+//  首页
+//  Created by chenlei on 16/1/27.
+//  Copyright © 2016年 laosiji. All rights reserved.
+//
+
+import UIKit
+
+class CLHomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
